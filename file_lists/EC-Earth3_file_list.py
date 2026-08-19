14ab870a219c6fed800befd026d96ece78b10264
Damien Irving <irving.damien@gmail.com>
2026-08-19 15:01:48 +1000

    Add EC-Earth file lists

--- file_lists/EC-Earth3_file_list.py
+++ file_lists/EC-Earth3_file_list.py
@@ -15,6 +15,13 @@ freq_dict = {
     'tasmax': 'day',
     'psl': 'day',
 }
+nfiles_dict = {
+    'pr': 165,  # 15*11
+    'sfcWind': 99, # 9*11
+    'tos': 110,  # 10*11
+    'tasmax': 165,
+    'psl': 165,
+}
 
 
 def create_file_list(var):
@@ -28,17 +35,22 @@ def create_file_list(var):
 
     # From 2005-2010 there are no r1i4p1f1 files, so i4 has been left out
     # 2018 is left out because it does not have i2 files
+    # sfcWind is missing s1976-r8i1p1f1 so I've left out r8
+    runs = [1, 2, 3, 4, 5, 6, 7, 9, 10] if var == 'sfcWind' else np.arange(1, 11)
+    freq = freq_dict[var]
+    nfiles = nfiles_dict[var]
     for year in np.arange(1960, 2017 + 1):
-        infiles1 = glob.glob(f"{file_dir}/s{year}-r?i1p1f1/day/{var}/gr/v202012??/*.nc")
-        infiles1.sort()
-        infiles2 = glob.glob(f"{file_dir}/s{year}-r10i1p1f1/day/{var}/gr/v202012??/*.nc")
-        infiles2.sort()
-        infiles3 = glob.glob(f"{file_dir}/s{year}-r?i2p1f1/day/{var}/gr/*/*.nc")
-        infiles3.sort()
-        infiles4 = glob.glob(f"{file_dir}/s{year}-r10i2p1f1/day/{var}/gr/*/*.nc")
-        infiles4.sort()
-        infiles = infiles1 + infiles2 + infiles3 + infiles4
-        assert len(infiles) == 165, f"year {year} does not have 165 (15*11) {var} files"
+        infiles = []
+        for run in runs:
+            infiles_i1 = glob.glob(f"{file_dir}/s{year}-r{run}i1p1f1/{freq}/{var}/gr/v202012??/*.nc")
+            infiles_i1.sort()
+            if var in ['sfcWind', 'tos']:
+                infiles_i2 = []
+            else:
+                infiles_i2 = glob.glob(f"{file_dir}/s{year}-r{run}i2p1f1/{freq}/{var}/gr/*/*.nc")
+                infiles_i2.sort()
+            infiles = infiles + infiles_i1 + infiles_i2
+        assert len(infiles) == nfiles, f"year {year} does not have {nfiles} {var} files"
         with open(outfile_name, "a") as outfile:
             for item in infiles:
                 outfile.write(f"{item}\n")
